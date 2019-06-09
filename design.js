38a2576e14b0ac4d307892f0f2a778bc567f3c3e
CarlosEAM <carlos.alford@gmail.com>
2019-06-09 17:59:44 +0100

    feat: Add Clear Canvas button to reset grid colour

--- design.js
+++ design.js
@@ -4,7 +4,6 @@
     1.1 to hide all the tools available. stops users from having to do much scrolling.
   2. retrieve colour in pixel by pressing it. Stops from having to use the colour wheel when
   the user wishes to use a colour they have already used.
-  3. Clear grid button. Clears the grid keeping the same N x M values use to make the grid.
   4. Colour palette for non standards browsers. Will need to research this one a bit.
   6. Grid should have limit depending on screen size??
 */
@@ -19,9 +18,12 @@ function initPixelArt() {
   // on form submit call makeGrid function and prevent default action
   let submitButton = $('form');
   submitButton.on('submit', false);
-  submitButton.on('submit',makeGrid);
+  submitButton.on('submit', makeGrid);
 
-  // set event listenet on the colour picker and update the brush colour to its most current value
+  // set event listener on clear button
+  $('.clear-canvas').on('click', clearGrid);
+
+  // set event listener on the colour picker and update the brush colour to its most current value
   let colorPicker = $('.color-picker');
   colorPicker.on('change', function(){ brushColor = $(this).prop('value'); });
 
@@ -58,6 +60,13 @@ function makeGrid() {
 
   // set event listeners on each pixel
   $('td').on('click', paintPixel);
+}
+
+/**
+* @description Clears the colours in the grid
+*/
+function clearGrid() {
+  makeGrid();
 }
 
 /**
